d57bfbb66824583278fcd9fd0f4bbc010a2bc8cc
godschisel <dominik.kotvan@gmail.com>
2015-12-05 14:01:24 +0100

    Read a Write funkcie pre AD prevodnik

---
+++ AD prevodnik/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments;
+using NationalInstruments.DAQmx;
+
+namespace AD_prevodnik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task analogInTask = new Task();
+            AIChannel myAIChannel;
+
+            myAIChannel = analogInTask.AIChannels.CreateVoltageChannel(
+                "dev1/ai0",
+                "myAIChannel",
+                AITerminalConfiguration.Differential,
+                0,
+                5,
+                AIVoltageUnits.Volts
+                );
+
+            AnalogSingleChannelReader reader = new AnalogSingleChannelReader(analogInTask.Stream);
+
+            double analogDataIn = reader.ReadSingleSample();
+
+            textBox1.Text = analogDataIn.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task analogOutTask = new Task();
+
+            AOChannel myAOChannel;
+
+            myAOChannel = analogOutTask.AOChannels.CreateVoltageChannel(
+                "dev1/ao0",
+                "myAOChannel",
+                0,
+                5,
+                AOVoltageUnits.Volts
+                );
+
+            AnalogSingleChannelWriter writer = new AnalogSingleChannelWriter(analogOutTask.Stream);
+
+            double analogDataOut;
+            analogDataOut = Convert.ToDouble(textBox2.Text);
+
+            writer.WriteSingleSample(true, analogDataOut);
+        }
+    }
+}
