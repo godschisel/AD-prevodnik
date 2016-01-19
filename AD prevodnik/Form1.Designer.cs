ead3d25a087c23de33b79818c13cbfa45f502094
godschisel <dominik.kotvan@gmail.com>
2016-01-19 17:21:27 +0100

    prerobene na presun do projektu skupiny

--- AD prevodnik/Form1.Designer.cs
+++ AD prevodnik/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(255, 215);
+            this.ClientSize = new System.Drawing.Size(304, 337);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
--- AD prevodnik/Form1.cs
+++ AD prevodnik/Form1.cs
@@ -19,13 +19,104 @@ namespace AD_prevodnik
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)  //Read Data
+        {
+            textBox1.Text = analogReadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)        // Write Data
+        {
+            analogWriteData(textBox2.Text);
+        }
+
+        string analogReadData()
+        {
+            Task analogInTask = new Task();
+            AIChannel myAIChannel;
+
+
+            myAIChannel = analogInTask.AIChannels.CreateVoltageChannel(
+                "dev1/ai1",
+                "myAIChannel",
+                AITerminalConfiguration.Differential,
+                0,
+                5,
+                AIVoltageUnits.Volts
+                );
+
+            AnalogSingleChannelReader reader = new AnalogSingleChannelReader(analogInTask.Stream);
+
+            double analogDataIn = reader.ReadSingleSample();
+
+            return analogDataIn.ToString();
+        }
+
+
+
+
+        void analogWriteData(string vstup)
+        {
+            Task analogOutTask = new Task();
+
+            AOChannel myAOChannel;
+
+            myAOChannel = analogOutTask.AOChannels.CreateVoltageChannel(
+                "dev1/ao1",
+                "myAOChannel",
+                0,
+                5,
+                AOVoltageUnits.Volts
+                );
+
+            AnalogSingleChannelWriter writer = new AnalogSingleChannelWriter(analogOutTask.Stream);
+
+            double analogDataOut = 0 ;
+
+            if (vstup != "")
+            {
+                try {
+                    analogDataOut = Convert.ToDouble(vstup);
+                }
+                catch (System.FormatException e) {
+                    MessageBox.Show("Vstup " + Convert.ToString( vstup) + " nie je v spravnom tvare.");
+                }
+            }
+            else
+            {
+                textBox2.Text = "0";
+                analogDataOut = Convert.ToDouble(textBox2.Text);
+            }
+            writer.WriteSingleSample(true, analogDataOut);
+        }
+    }
+}
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+
+
+namespace AD_prevodnik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //Read Data
         {
             Task analogInTask = new Task();   
             AIChannel myAIChannel;
+            
 
             myAIChannel = analogInTask.AIChannels.CreateVoltageChannel(
-                "dev1/ai0",
+                "dev1/ai1",
                 "myAIChannel",
                 AITerminalConfiguration.Differential,
                 0,
@@ -40,14 +131,14 @@ namespace AD_prevodnik
             textBox1.Text = analogDataIn.ToString();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)        // Write Data
         {
             Task analogOutTask = new Task();
 
             AOChannel myAOChannel;
 
             myAOChannel = analogOutTask.AOChannels.CreateVoltageChannel(
-                "dev1/ao0",
+                "dev1/ao1",
                 "myAOChannel",
                 0,
                 5,
@@ -57,9 +148,20 @@ namespace AD_prevodnik
             AnalogSingleChannelWriter writer = new AnalogSingleChannelWriter(analogOutTask.Stream);
 
             double analogDataOut;
-            analogDataOut = Convert.ToDouble(textBox2.Text);
+
+            if (textBox2.Text != "")
+            {
+                analogDataOut = Convert.ToDouble(textBox2.Text);
+            }
+            else {
+                textBox2.Text = "0";
+                analogDataOut = Convert.ToDouble(textBox2.Text);
+            }
 
             writer.WriteSingleSample(true, analogDataOut);
         }
+
+        
     }
 }
+*/
